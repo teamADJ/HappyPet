242aca094a3167cba0138ec59049756a8d2d4786
hmm <jackyxonyu00@gmail.com>
2021-02-19 22:50:58 +0700

    login register admin

---
+++ app/src/main/java/com/adj/happypet/Model/Admin.java
@@ -0,0 +1,39 @@
+package com.adj.happypet.Model;
+
+public class Admin {
+
+    String fullname,email,password;
+
+    public Admin() {
+    }
+
+    public Admin(String fullname, String email, String password) {
+        this.fullname = fullname;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
